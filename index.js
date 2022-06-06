83bc3b3b0ca96518d080a6c0fb06cd88adcbabdf
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-06 04:23:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    var cards = []
+    for(var i = 0; i < names.length; i++) {
+    cards.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+ }
+   return cards;
+}
+
+function countDown() {
+   var countdown = 10;
+   while (countdown >= 0) {
+       console.log(countdown--);
+   }
+}
